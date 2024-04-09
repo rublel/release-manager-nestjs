e3e953902ffa7dabf344dc770e960b85d5734adc
rubenlel <rlellouche_ext@christiandior.com>
2024-04-09 21:25:25 +0300

    Update server port to use environment variable or default to 3001

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
   Logger.verbose(`Server running on http://${ip.address()}:3001`, 'Bootstrap');
 }
 bootstrap();
